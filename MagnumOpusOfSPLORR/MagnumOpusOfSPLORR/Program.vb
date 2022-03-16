474b5ee46275097867c38b2c6ef889f1ee193851
ernestpazera <ernestpazera@gmail.com>
2022-03-16 17:46:16 -0500

    a simple hello world

---
+++ MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Program.vb
@@ -0,0 +1,19 @@
+Imports System
+Imports Spectre.Console
+
+Module Program
+    Sub Main(args As String())
+        Console.Title = "Magnum Opus of SPLORR!!"
+        AnsiConsole.MarkupLine("[aqua]***************************[/]")
+        AnsiConsole.MarkupLine("[aqua]* Magnum Opus of SPLORR!! *[/]")
+        AnsiConsole.MarkupLine("[aqua]***************************[/]")
+        AnsiConsole.MarkupLine("[gray]A Production of TheGrumpyGameDev[/]")
+        AnsiConsole.MarkupLine("[gray]...With ""help"" from his ""friends""[/]")
+        AnsiConsole.WriteLine()
+        Dim prompt As New SelectionPrompt(Of String) With {
+            .Title = "[olive]Main Menu:[/]"
+        }
+        prompt.AddChoice("Quit")
+        AnsiConsole.Prompt(prompt)
+    End Sub
+End Module
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# MagnumOpusOfSPLORR
+# Magnum Opus Of SPLORR!!
+
+## Deliberate Decisions
+
+- UI is Spectre.Console
+- Data Store is SQLite
