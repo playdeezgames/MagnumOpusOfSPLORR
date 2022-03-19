2f475e104b93c834f6efc577362d6869cbc6ad94
TheGrumpyGameDev <ernestpazera@msn.com>
2022-03-19 06:14:50 -0500

    i can save the game

--- MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Embark.vb
+++ MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Embark.vb
@@ -1,9 +1,11 @@
-﻿Imports MOOS.Game
+﻿Imports MOOS.Data
+Imports MOOS.Game
 Imports Spectre.Console
 
 Module Embark
     Private Const AbandonGameText = "Abandon Game"
     Const NeverMindText = "Never Mind"
+    Private Const SaveGameText = "Save Game"
     Private Function ConfirmAbandon() As Boolean
         Dim prompt = New ConfirmationPrompt("Are you sure you want to abandon the game?")
         Return AnsiConsole.Prompt(prompt)
@@ -13,17 +15,28 @@ Module Embark
             {
             .Title = "[olive]Game Menu:[/]"
             }
-        prompt.AddChoice(AbandonGameText)
         prompt.AddChoice(NeverMindText)
+        prompt.AddChoice(SaveGameText)
+        prompt.AddChoice(AbandonGameText)
         Select Case AnsiConsole.Prompt(prompt)
             Case AbandonGameText
-                Return ConfirmAbandon
+                Return ConfirmAbandon()
+            Case SaveGameText
+                HandleSaveGame()
+                Return False
             Case NeverMindText
                 Return False
             Case Else
                 Throw New NotImplementedException
         End Select
     End Function
+
+    Private Sub HandleSaveGame()
+        AnsiConsole.WriteLine()
+        Dim fileName = AnsiConsole.Ask(Of String)("Filename:")
+        Store.Save(fileName)
+    End Sub
+
     Private Const GameMenuText = "Game Menu"
     Sub Run()
         Game.Start()
