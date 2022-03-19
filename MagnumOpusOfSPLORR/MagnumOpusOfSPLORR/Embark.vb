a0d2a2aa96d8db14dfb30f945c90ffe94c3b0973
TheGrumpyGameDev <ernestpazera@msn.com>
2022-03-19 05:50:25 -0500

    game abandoning and setting up the projects correctly to have option strict and all warnings as errors

--- MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Embark.vb
+++ MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Embark.vb
@@ -1,8 +1,50 @@
-﻿Imports Spectre.Console
+﻿Imports MOOS.Game
+Imports Spectre.Console
 
 Module Embark
+    Private Const AbandonGameText = "Abandon Game"
+    Const NeverMindText = "Never Mind"
+    Private Function ConfirmAbandon() As Boolean
+        Dim prompt = New ConfirmationPrompt("Are you sure you want to abandon the game?")
+        Return AnsiConsole.Prompt(prompt)
+    End Function
+    Private Function HandleGameMenu() As Boolean
+        Dim prompt As New SelectionPrompt(Of String) With
+            {
+            .Title = "[olive]Game Menu:[/]"
+            }
+        prompt.AddChoice(AbandonGameText)
+        prompt.AddChoice(NeverMindText)
+        Select Case AnsiConsole.Prompt(prompt)
+            Case AbandonGameText
+                Return ConfirmAbandon
+            Case NeverMindText
+                Return False
+            Case Else
+                Throw New NotImplementedException
+        End Select
+    End Function
+    Private Const GameMenuText = "Game Menu"
     Sub Run()
+        Game.Start()
         AnsiConsole.WriteLine()
         AnsiConsole.MarkupLine("So, you embarked. Good for you!")
+        Dim done = False
+        While Not done
+            AnsiConsole.WriteLine()
+            AnsiConsole.MarkupLine("You exist!")
+            Dim prompt As New SelectionPrompt(Of String) With
+                {
+                    .Title = "[olive]Now what?[/]"
+                }
+            prompt.AddChoice(GameMenuText)
+            Select Case AnsiConsole.Prompt(prompt)
+                Case GameMenuText
+                    done = HandleGameMenu()
+                Case Else
+                    Throw New NotImplementedException
+            End Select
+        End While
+        Game.Finish()
     End Sub
 End Module
