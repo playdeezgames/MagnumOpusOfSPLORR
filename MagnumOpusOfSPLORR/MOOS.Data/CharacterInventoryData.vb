0a183018e9f26097f5e4df675e2853c07460e598
TheGrumpyGameDev <ernestpazera@msn.com>
2022-03-27 06:36:29 -0500

    data layer for character inventory

---
+++ MagnumOpusOfSPLORR/MOOS.Data/CharacterInventoryData.vb
@@ -0,0 +1,33 @@
+﻿Public Module CharacterInventoryData
+    Friend Sub Initialize()
+        CharacterData.Initialize()
+        InventoryData.Initialize()
+        ExecuteNonQuery(
+            "CREATE TABLE IF NOT EXISTS [CharacterInventories]
+            (
+                [CharacterId] INT NOT NULL UNIQUE,
+                [InventoryId] INT NOT NULL,
+                FOREIGN KEY ([CharacterId]) REFERENCES [Characters]([CharacterId]),
+                FOREIGN KEY ([InventoryId]) REFERENCES [Inventories]([InventoryId])
+            );")
+    End Sub
+    Function Read(characterId As Long) As Long?
+        Initialize()
+        Return ExecuteScalar(Of Long)(
+            "SELECT [InventoryId] FROM [CharacterInventories] WHERE [CharacterId]=@CharacterId;",
+            MakeParameter("@CharacterId", characterId))
+    End Function
+    Sub Write(characterId As Long, inventoryId As Long)
+        Initialize()
+        ExecuteNonQuery(
+            "REPLACE INTO [CharacterInventories]([CharacterId],[InventoryId]) VALUES (@CharacterId,@InventoryId);",
+            MakeParameter("@CharacterId", characterId),
+            MakeParameter("@InventoryId", inventoryId))
+    End Sub
+    Friend Sub Clear(characterId As Long)
+        Initialize()
+        ExecuteNonQuery(
+            "DELETE FROM [CharacterInventories] WHERE [CharacterId]=@CharacterId;",
+            MakeParameter("@CharacterId", characterId))
+    End Sub
+End Module
