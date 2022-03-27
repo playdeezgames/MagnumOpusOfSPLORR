37312c3c30f5f8229d19e9ab294353ee867ba1c0
TheGrumpyGameDev <ernestpazera@msn.com>
2022-03-27 11:44:27 -0500

    direction editors are refactored

--- MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Edit/Direction/EditDirectionMenu.vb
+++ MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Edit/Direction/EditDirectionMenu.vb
@@ -1,20 +1,23 @@
-﻿Imports MOOS.Game
-Imports Spectre.Console
-
-Module EditDirectionMenu
+﻿Module EditDirectionMenu
+    Private Sub ShowStatus(direction As Direction)
+        AnsiConsole.MarkupLine($"Id: {direction.Id}")
+        AnsiConsole.MarkupLine($"Name: {direction.Name}")
+    End Sub
+    Private Function CreatePrompt(direction As Direction) As SelectionPrompt(Of String)
+        Dim prompt = New SelectionPrompt(Of String) With {.Title = "[olive]Now what?[/]"}
+        prompt.AddChoice(GoBackText)
+        prompt.AddChoice(ChangeNameText)
+        If direction.CanDestroy Then
+            prompt.AddChoice(DestroyText)
+        End If
+        Return prompt
+    End Function
     Sub Run(direction As Direction)
         Dim done = False
         While Not done
             AnsiConsole.Clear()
-            AnsiConsole.MarkupLine($"Id: {direction.Id}")
-            AnsiConsole.MarkupLine($"Name: {direction.Name}")
-            Dim prompt = New SelectionPrompt(Of String) With {.Title = "[olive]Now what?[/]"}
-            prompt.AddChoice(GoBackText)
-            prompt.AddChoice(ChangeNameText)
-            If direction.CanDestroy Then
-                prompt.AddChoice(DestroyText)
-            End If
-            Select Case AnsiConsole.Prompt(prompt)
+            ShowStatus(direction)
+            Select Case AnsiConsole.Prompt(CreatePrompt(direction))
                 Case GoBackText
                     done = True
                 Case ChangeNameText
--- MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Edit/Direction/EditDirectionsMenu.vb
+++ MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Edit/Direction/EditDirectionsMenu.vb
@@ -3,17 +3,20 @@ Imports Spectre.Console
 
 Module EditDirectionsMenu
     Private Const CreateDirectionText = "Create Direction"
+    Private Function CreatePrompt() As SelectionPrompt(Of String)
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = "[olive]Edit Directions:[/]"}
+        prompt.AddChoice(GoBackText)
+        prompt.AddChoice(CreateDirectionText)
+        For Each direction In Directions.AllDirections
+            prompt.AddChoices(direction.UniqueName)
+        Next
+        Return prompt
+    End Function
     Sub Run()
         Dim done = False
         While Not done
             AnsiConsole.Clear()
-            Dim prompt As New SelectionPrompt(Of String) With {.Title = "[olive]Edit Directions:[/]"}
-            prompt.AddChoice(GoBackText)
-            prompt.AddChoice(CreateDirectionText)
-            For Each direction In Directions.AllDirections
-                prompt.AddChoices(direction.UniqueName)
-            Next
-            Dim answer = AnsiConsole.Prompt(prompt)
+            Dim answer = AnsiConsole.Prompt(CreatePrompt)
             Select Case answer
                 Case GoBackText
                     done = True
