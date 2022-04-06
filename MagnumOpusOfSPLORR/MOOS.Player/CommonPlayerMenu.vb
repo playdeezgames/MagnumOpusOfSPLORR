a82ed72a4e4d2784c55b14ebe01cb0aa4d562508
ernestpazera <ernestpazera@gmail.com>
2022-04-06 04:22:28 -0500

    moved the item type uniquename picker to editor instead of player which is where it always belonged

--- MagnumOpusOfSPLORR/MOOS.Player/CommonPlayerMenu.vb
+++ MagnumOpusOfSPLORR/MOOS.Player/CommonPlayerMenu.vb
@@ -24,21 +24,4 @@
                 Return FindItemTypeByName(groups.FirstOrDefault(Function(x) x.Key = answer).Key).First
         End Select
     End Function
-    Public Function ChooseItemTypeUniqueNameFromInventory(title As String, canCancel As Boolean, inventory As Inventory) As ItemType 'both
-        Dim prompt As New SelectionPrompt(Of String) With {.Title = $"[olive]{title}[/]"}
-        If canCancel Then
-            prompt.AddChoice(NeverMindText)
-        End If
-        Dim groups = inventory.Items.GroupBy(Function(x) x.ItemType.UniqueName)
-        For Each itemType In groups
-            prompt.AddChoices(itemType.Key)
-        Next
-        Dim answer = AnsiConsole.Prompt(prompt)
-        Select Case answer
-            Case NeverMindText
-                Return Nothing
-            Case Else
-                Return FindItemTypeByUniqueName(groups.FirstOrDefault(Function(x) x.Key = answer).Key)
-        End Select
-    End Function
 End Module
--- MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Utility/CommonEditorMenu.vb
+++ MagnumOpusOfSPLORR/MagnumOpusOfSPLORR/Utility/CommonEditorMenu.vb
@@ -94,4 +94,21 @@
         Loop Until RNG.ValidateDice(damageDice)
         Return damageDice
     End Function
+    Public Function ChooseItemTypeUniqueNameFromInventory(title As String, canCancel As Boolean, inventory As Inventory) As ItemType 'both
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = $"[olive]{title}[/]"}
+        If canCancel Then
+            prompt.AddChoice(NeverMindText)
+        End If
+        Dim groups = inventory.Items.GroupBy(Function(x) x.ItemType.UniqueName)
+        For Each itemType In groups
+            prompt.AddChoices(itemType.Key)
+        Next
+        Dim answer = AnsiConsole.Prompt(prompt)
+        Select Case answer
+            Case NeverMindText
+                Return Nothing
+            Case Else
+                Return FindItemTypeByUniqueName(groups.FirstOrDefault(Function(x) x.Key = answer).Key)
+        End Select
+    End Function
 End Module
